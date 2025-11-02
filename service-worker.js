f2781e8506c6493a85f5ed4c36784674427caf1c
darkduck9 <60059377+darkduck9@users.noreply.github.com>
2025-11-02 21:35:42 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-s
 
 const versions = {
     images: 'v0.53',
-    js: 'v0.71',
+    js: 'v0.72',
     css: 'v0.60'
 };
 
@@ -75,3 +75,4 @@ self.addEventListener('activate', event => {
         })
     );
 });
+
